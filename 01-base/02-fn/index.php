17d8b7daba870cd9749b0d21c29d5add8d25567a
huanggengzhong <915835528@qq.com>
2020-12-24 00:01:43 +0800

    初始化

---
+++ 01-base/02-fn/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>函数的使用</title>
+</head>
+
+<body>
+  <?php
+// 定义函数 
+  // 1.定义要求:同一块作用域中名字不能相同;函数名称不区分大小写,而常量和变量区分大小写;
+
+  // 2.自定义函数
+   function example($num){
+     return $num *$num;
+   }
+
+   echo example(10);//打印:100
+   echo "<br>";
+
+  //  作用域
+
+  $zy="你好";
+  $zyy="PHP语言";//全局变量
+  function lxt(){
+    $zy="我喜欢";
+    echo $zy;//打印局部变量
+    global $zyy;//使用全局变量要加global关键字
+    echo $zyy;
+  }
+  lxt();//打印我喜欢PHP语言
+
+
+  // 静态变量和普通变量的区别:静态变量会有缓存上一次数据
+  function zdy(){
+   static $message=0;//第二次不会重新赋值,会缓存上一次值
+   $message+=1;
+   echo $message." ";//点是拼接符
+ }
+
+ function changFn(){
+   $message=0;
+   $message+=1;
+   echo $message." ";
+ }
+ echo "<br>";
+
+ for($i=0;$i<10;$i++) zdy();//循环执行10次,打印:1 2 3 4 5 6 7 8 9 10
+ echo "<br>";
+ for($i=0;$i<10;$i++) changFn();//循环执行10次,打印:1 1 1 1 1 1 1 1 1 1
+
+  ?>
+</body>
+
+</html>
